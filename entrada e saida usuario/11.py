96102d5f7488a97d0e73c6a6acc64b7ddf1ed237
DiasEdoardo <eduardodiasmachado7979@gmail.com>
2023-08-29 12:46:15 -0300

    Python

---
+++ entrada e saida usuario/11.py
@@ -0,0 +1,5 @@
+n1 = float(input('Qual a primeira nota de peso 2?\n'));
+n2 = float(input('Qual a segunda nota de peso 3?\n'));
+n3 = float (input('Qual a terceira nota de peso 5?\n'));
+media=n1*2+n2*3+n3*5/10;
+print('a media ponderada dessas 3 notas é :',media);
